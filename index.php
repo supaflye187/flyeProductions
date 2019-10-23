9724e24aae81a0fe12cac3edeaf8af31ccd169f9
Eric Flye <supaflye187@Sq1.attlocal.net>
2019-10-23 15:21:00 -0500

    adding index.php page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <?php
+    require './resources/config.php';
+     require_once('./resources/templates/header.php');
+     require_once('./resources/templates/modal.php');
+     //require_once('./resources/templates/home.php');
+    //require_once('./resources/templates/about.php');
+    require_once('./resources/templates/gallery.php');
+    //require_once('./resources/templates/contact.php');
+     require_once('./resources/templates/footer.php');
+  ?>
+</html>
